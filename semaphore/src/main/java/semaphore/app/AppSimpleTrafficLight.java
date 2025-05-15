63c841f15e5f83384685b449aa2cc3d871ca68e6
Rogerio <ryokomizo0@gmail.com>
2025-05-14 23:45:57 -0300

    14/05/2025 - Aula10B - Classe SimpleTrafficLight concluída

---
+++ semaphore/src/main/java/semaphore/app/AppSimpleTrafficLight.java
@@ -0,0 +1,23 @@
+package semaphore.app;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.io.IOException;
+
+import semaphore.trafficLight.simple.SimpleTrafficLight;
+import semaphore.util.gui.MyWindow;
+
+public class AppSimpleTrafficLight {
+
+	public static void main(String[] args) throws IOException{
+	
+		SimpleTrafficLight defaultOne = new SimpleTrafficLight();
+		
+		Point position = new Point(100,100);
+		Dimension dimension = new Dimension(100,350);
+		SimpleTrafficLight customOne = new SimpleTrafficLight(position,dimension);
+		
+		MyWindow window = new MyWindow();
+		window.add(defaultOne);
+		window.add(customOne);
+}
--- semaphore/src/main/java/semaphore/trafficLight/simple/SimpleTrafficLight.java
+++ semaphore/src/main/java/semaphore/trafficLight/simple/SimpleTrafficLight.java
@@ -1,12 +1,17 @@
 package semaphore.trafficLight.simple;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Point;
 import java.io.IOException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+
+import semaphore.light.e27.E27LightBulb;
 import semaphore.spot.SpotLight;
 
-public class SimpleTrafficLight {
+public class SimpleTrafficLight implements Paintable{
 	
 	private Point position = new Point(0,0);
 	private Dimension dimension = new Dimension(70,180);
@@ -15,13 +20,96 @@ public class SimpleTrafficLight {
 	private SpotLight green;
 	private SpotLight red;
 	
-	private void create(){
-		//to do
+	private String currentRelativePath() {
+		
+		return "/" + 
+				this.getClass()
+				.getPackageName()
+				.toString()
+				.replace('.','/')+
+				"/";
 	}
+	
+	private SpotLight createSpot( String color ) throws IOException{
+		
+		final String path = currentRelativePath() + "img/";
+		URL url;
+		
+		url = getClass().getResource(path + color +"On.png");
+		Image maskOn = ImageIO.read(url);
+		
+		url = getClass().getResource(path + color +"Off.png");
+		Image maskOff = ImageIO.read(url);
+		
+		SpotLight spot = new SpotLight(maskOn,maskOff);
+		spot.setPosition(xLeft, yTop);
+		spot.setSize(width, height);
+		
+		return spot;
+	}
+	
+	//--Paintable Implementation-------------
+		@Override
+		public void paint(Graphics g) {
+			
+			synchronized (g) {
+				
+				int xLeft = this.position.x;
+				int yTop = this.position.y;
+				int width = this.dimension.width;
+				int height = this.dimension.height;
+				
+				g.drawImage(null, xLeft, yTop, width, height, null);
+				
+				this.green.paint(g);
+				this.yellow.paint(g);
+				this.red.paint(g);
+			}
+		}
+	
+	
+	private void create() throws IOException{
+	
+		this.green = createSpot("green");
+		this.green.setLight(new E27LightBulb());
+		
+		this.yellow = createSpot("yellow");
+		this.yellow.setLight(new E27LightBulb());
+		
+		this.red = createSpot("red");
+		this.red.setLight(new E27LightBulb());
+		
+		String path = this.currentRelativePath();
+		URL url = this.getClass().getResource(path + "img/trafficLight.png");
+		this.mask = ImageIO.read(url);
+		
+	}
+	
+	private void configurePosition() {
+			
+		final int WIDTH = ( this.dimension.width - 20);
+		final int HEIGHT = ((this.dimension.height -30 ) / 3);
+		final Dimension dimension = new Dimension(WIDTH,HEIGHT);
+		
+		int xLeft = this.position.x + 10;
+		int yTop = this.position.y + 10;
+		this.green.setPosition(xLeft, yTop);
+		this.green.setDimension(dimension);
+		
+		yTop = (yTop + 5 + HEIGHT);
+		this.yellow.setPosition(xLeft, yTop);
+		this.yellow.setDimension(dimension);
+
+		yTop = (yTop + 5 + HEIGHT);
+		this.red.setPosition(xLeft, yTop);
+		this.red.setDimension(dimension);
+	}
+	
 	
 	public SimpleTrafficLight() throws IOException{
 		
 		this.create();
+		this.configurePosition();
 	}
 	
 	public SimpleTrafficLight(Point position,
@@ -31,10 +119,13 @@ public class SimpleTrafficLight {
 		this.dimension = dimension;
 		
 		this.create();
-		}
+		this.configurePosition();
+		
+	}
 		
 	public void setPosition(Point position) {
 		this.position = new Point(position);
+		this.configurePosition();
 	}
 	
 	public Point getPosition() {
@@ -43,6 +134,7 @@ public class SimpleTrafficLight {
 	
 	public void setDimension(Dimension dimension) {
 		this.dimension = new Dimension(dimension);
+		this.configurePosition();
 	}
 	
 	public Dimension getDimension() {
@@ -50,4 +142,5 @@ public class SimpleTrafficLight {
 	}
 	
 	
+		
 }
